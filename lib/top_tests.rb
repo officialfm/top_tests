29e48f314892f74f597c03b27c18013cc6ad88ca
Alexis Bernard <alexis@official.fm>
2011-12-12 16:34:54 +0100

    Top tests breaks your build if any of your tests exceed the max duration.

--- lib/top_tests.rb
+++ lib/top_tests.rb
@@ -10,7 +10,7 @@ module TopTests
     klass.setup :start_timer
     klass.teardown :stop_timer
     klass.extend(ClassMethods)
-    at_exit { at_exit { klass.print_top_tests } }
+    at_exit { at_exit { klass.at_exit_callback } }
   end
 
   ########################
@@ -35,16 +35,45 @@ module TopTests
       @@tests_durations ||= []
     end
 
+    def max_duration=(seconds)
+      @@max_duration = seconds
+    end
+
+    def max_duration
+      @@max_duration
+    end
+
     def top_tests
       tests_durations.sort { |a, b| b.last <=> a.last }
     end
 
+    def slow_tests
+      max_duration ? top_tests.find_all { |t| top_tests.first.last > max_duration } : []
+    end
+
+    def format_tests(tests)
+      tests.map { |t| "  #{format("%7.3f", t.last)} #{t.first}" }.join("\n")
+    end
+
     def print_top_tests
       puts "\nTop tests:"
-      puts top_tests.shift(10).map { |t| "#{format("%7.3f", t.last)} #{t.first}" }.join("\n")
+      puts format_tests(top_tests.shift(10))
       puts
+    end
+
+    def check_tests_duration
+      if !slow_tests.empty?
+        puts "\nTEST?FAIL! #{slow_tests.count} test(s) are taking longer than #{max_duration} seconds:"
+        puts format_tests(top_tests)
+        puts
+        exit 1
+      end
+    end
+
+    def at_exit_callback
+      check_tests_duration
+      print_top_tests
     end
   end
 
 end
-
